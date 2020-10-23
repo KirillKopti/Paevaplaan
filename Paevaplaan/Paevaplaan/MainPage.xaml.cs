7c68284221f35ae3988a1405d06e8982b975699e
opilane <opilane@HP-CZC6297B98>
2020-10-23 11:25:16 +0300

    Add project files.

---
+++ Paevaplaan/Paevaplaan/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Paevaplaan
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            StackLayout stack = new StackLayout();
+            Button esmas_btn = new Button { Text = "Esmaspäev" };
+            stack.Children.Add(esmas_btn);
+            esmas_btn.Clicked += new EventHandler(Batton_Clicked);
+
+            Content = stack;
+        }
+
+        private async void Batton_Clicked(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            switch (btn.Text)
+            {
+                case "Esmaspäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Esmaspaev());
+                    break;
+
+            }
+        }
+     }
+}
