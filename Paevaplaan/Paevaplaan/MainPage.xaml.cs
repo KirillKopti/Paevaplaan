00347c7c180dfeb5d7b4650a5e6a4f95a9b91616
opilane <opilane@HP-CZC6297B98>
2020-10-23 11:46:26 +0300

    vanna

--- Paevaplaan/Paevaplaan/MainPage.xaml.cs
+++ Paevaplaan/Paevaplaan/MainPage.xaml.cs
@@ -18,6 +18,42 @@ namespace Paevaplaan
             esmas_btn.Clicked += new EventHandler(Batton_Clicked);
 
             Content = stack;
+
+            Button teisi_btn = new Button { Text = "Teisipäev" };
+            stack.Children.Add(teisi_btn);
+            teisi_btn.Clicked += new EventHandler(Batton_Clicked);
+
+            Content = stack;
+
+            Button kolma_btn = new Button { Text = "Kolmapäev" };
+            stack.Children.Add(kolma_btn);
+            kolma_btn.Clicked += new EventHandler(Batton_Clicked);
+
+            Content = stack;
+
+            Button nelja_btn = new Button { Text = "Neljapäev" };
+            stack.Children.Add(nelja_btn);
+            nelja_btn.Clicked += new EventHandler(Batton_Clicked);
+
+            Content = stack;
+
+            Button re_btn = new Button { Text = "Rede" };
+            stack.Children.Add(re_btn);
+            re_btn.Clicked += new EventHandler(Batton_Clicked);
+
+            Content = stack;
+
+            Button lau_btn = new Button { Text = "Laupäev" };
+            stack.Children.Add(lau_btn);
+            lau_btn.Clicked += new EventHandler(Batton_Clicked);
+
+            Content = stack;
+
+            Button puha_btn = new Button { Text = "Pühapäev" };
+            stack.Children.Add(puha_btn);
+            puha_btn.Clicked += new EventHandler(Batton_Clicked);
+
+            Content = stack;
         }
 
         private async void Batton_Clicked(object sender, EventArgs e)
@@ -30,6 +66,35 @@ namespace Paevaplaan
                     await Navigation.PushAsync(new Esmaspaev());
                     break;
 
+                case "Teisipäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Teisipaev());
+                    break;
+
+                case "Kolmapäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Kolmapaev());
+                    break;
+
+                case "Neljapäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Neljapaev());
+                    break;
+
+                case "Rede":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Rede());
+                    break;
+
+                case "Laupäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Laupaev());
+                    break;
+
+                case "Pühapäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Puhapaev());
+                    break;
             }
         }
      }
